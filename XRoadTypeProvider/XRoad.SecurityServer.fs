f9d89fd7069025ecd12e62f9d48fe89105760b5b
Janno Põldma <janno.poldma@gmail.com>
2015-02-05 14:28:22 +0200

    Refactoring.

---
+++ XRoadTypeProvider/XRoad.SecurityServer.fs
@@ -0,0 +1,51 @@
+﻿module internal XRoad.SecurityServer
+
+open System.Xml
+open System.Xml.Linq
+open XRoadTypeProvider.Runtime
+open XRoadTypeProvider.Wsdl
+
+type Producers =
+  { Name: string
+    WsdlUri: string
+    Description: string }
+
+let discoverProducers serverIP =
+    let serverUri = sprintf "http://%s/cgi-bin/consumer_proxy" serverIP
+
+    let doc =
+        let request = XRoadRequest.initRequest(serverUri)
+        (   use stream = request.GetRequestStream() in
+            use writer = XmlWriter.Create(stream)
+            writer.WriteStartDocument()
+            writer.WriteStartElement("SOAP-ENV", "Envelope", XmlNamespace.SoapEnvelope)
+
+            writer.WriteStartElement("Body", XmlNamespace.SoapEnvelope)
+            writer.WriteStartElement("listProducers", XmlNamespace.XRoad)
+            writer.WriteEndElement()
+            writer.WriteEndElement()
+
+            writer.WriteEndElement()
+            writer.WriteEndDocument())
+        use resp = request.GetResponse()
+        use reader = new System.IO.StreamReader(resp.GetResponseStream())
+        XDocument.Load(reader)
+
+    let wsdlPrefix =
+        match serverUri.EndsWith("/consumer_proxy") with
+        | true -> serverUri.Substring(0, serverUri.LastIndexOf("consumer_proxy")) + "uriproxy?producer="
+        | _ -> failwith "Invalid XRoad security server uri!"
+
+    let envelope = doc.Elements(XName.Get("Envelope", XmlNamespace.SoapEnvelope)) |> Seq.exactlyOne
+    let body = envelope.Elements(XName.Get("Body", XmlNamespace.SoapEnvelope)) |> Seq.exactlyOne
+    let message = body.Elements(XName.Get("listProducersResponse", XmlNamespace.XRoad)) |> Seq.exactlyOne
+    let response = message.Elements(XName.Get("response"))
+
+    response.Elements(XName.Get("item"))
+    |> Seq.map (fun item ->
+        let oneValue es = es |> Seq.map (fun (e: XElement) -> e.Value) |> Seq.exactlyOne
+        let name = oneValue(item.Elements(XName.Get("name")))
+        { Name = name
+          WsdlUri = sprintf "http://%s/cgi-bin/uriproxy?producer=%s" serverIP name
+          Description = oneValue(item.Elements(XName.Get("description"))) })
+    |> List.ofSeq
--- XRoadTypeProvider/XRoadProviders.fs
+++ XRoadTypeProvider/XRoadProviders.fs
@@ -12,15 +12,37 @@ type XRoadProviders(config: TypeProviderConfig) as this =
     let namespaceName = "XRoad.Providers"
     let baseTy = typeof<obj>
 
-    let providerTy = ProvidedTypeDefinition(theAssembly, namespaceName, "XRoadService", Some baseTy, IsErased=false)
-    let providedAssembly = ProvidedAssembly(Path.Combine(Path.GetTempPath(), "XRoadService.dll"))
+    let producerTy = ProvidedTypeDefinition(theAssembly, namespaceName, "XRoadProducer", Some baseTy, IsErased=false)
+    let producerAssembly = ProvidedAssembly(Path.ChangeExtension(Path.GetTempFileName(), "dll"))
 
-    do providerTy.DefineStaticParameters(
-        [ProvidedStaticParameter("ProducerUri", typeof<string>)],
-        fun typeName parameterValues ->
-            let thisTy = ProvidedTypeDefinition(theAssembly, namespaceName, typeName, Some baseTy, IsErased=false)
-            providedAssembly.AddTypes([thisTy])
-            thisTy
-        )
+    let serverTy = ProvidedTypeDefinition(theAssembly, namespaceName, "XRoadServer", Some baseTy)
 
-    do this.AddNamespace(namespaceName, [providerTy])
+    do
+        producerTy.DefineStaticParameters(
+            [ProvidedStaticParameter("ProducerUri", typeof<string>)],
+            fun typeName parameterValues ->
+                let thisTy = ProvidedTypeDefinition(theAssembly, namespaceName, typeName, Some baseTy, IsErased=false)
+                producerAssembly.AddTypes([thisTy])
+                thisTy)
+
+        serverTy.DefineStaticParameters(
+            [ProvidedStaticParameter("ServerIP", typeof<string>)],
+            fun typeName parameterValues ->
+                let thisTy = ProvidedTypeDefinition(theAssembly, namespaceName, typeName, Some baseTy)
+                match parameterValues with
+                | [| :? string as serverIP |] ->
+                    let producersTy = ProvidedTypeDefinition("Producers", Some baseTy, HideObjectMethods=true)
+                    producersTy.AddXmlDoc("List of available database names registered at the security server.")
+                    thisTy.AddMember(producersTy)
+                    XRoad.SecurityServer.discoverProducers(serverIP)
+                    |> List.map (fun producer ->
+                        let producerTy = ProvidedTypeDefinition(producer.Name, Some baseTy, HideObjectMethods=true)
+                        producerTy.AddMember(ProvidedLiteralField("ProducerName", typeof<string>, producer.Name))
+                        producerTy.AddMember(ProvidedLiteralField("WsdlUri", typeof<string>, producer.WsdlUri))
+                        producerTy.AddXmlDoc(producer.Description)
+                        producerTy)
+                    |> producersTy.AddMembers
+                | _ -> failwith "Unexpected parameter values!"
+                thisTy)
+
+    do this.AddNamespace(namespaceName, [producerTy; serverTy])
