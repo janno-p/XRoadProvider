ceccf0d74aa54f7b842b180357c6473eb33682aa
Janno Põldma <janno.poldma@gmail.com>
2015-02-05 12:13:06 +0200

    Generated type provider instead of erased type.

---
+++ XRoadTypeProvider/XRoadProviders.fs
@@ -0,0 +1,26 @@
+﻿module XRoadProviders
+
+open Microsoft.FSharp.Core.CompilerServices
+open ProviderImplementation.ProvidedTypes
+open System.IO
+
+[<TypeProvider>]
+type XRoadProviders(config: TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces()
+
+    let theAssembly = typeof<XRoadProviders>.Assembly
+    let namespaceName = "XRoad.Providers"
+    let baseTy = typeof<obj>
+
+    let providerTy = ProvidedTypeDefinition(theAssembly, namespaceName, "XRoadService", Some baseTy, IsErased=false)
+    let providedAssembly = ProvidedAssembly(Path.Combine(Path.GetTempPath(), "XRoadService.dll"))
+
+    do providerTy.DefineStaticParameters(
+        [ProvidedStaticParameter("ProducerUri", typeof<string>)],
+        fun typeName parameterValues ->
+            let thisTy = ProvidedTypeDefinition(theAssembly, namespaceName, typeName, Some baseTy, IsErased=false)
+            providedAssembly.AddTypes([thisTy])
+            thisTy
+        )
+
+    do this.AddNamespace(namespaceName, [providerTy])
