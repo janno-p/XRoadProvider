5479e6ba5bc7aa2ca3fc542ad2dd2e7e91710e45
Janno Põldma <janno.poldma@gmail.com>
2015-08-22 08:46:36 +0300

    Add test project for XRoad serializer.

---
+++ src/XRoadSerializer/Serializer.fs
@@ -0,0 +1,9 @@
+﻿namespace XRoad
+
+open System.Xml
+
+type Serializer() =
+    member __.Deserialize(_: XmlReader) : 'T =
+        null
+    member __.Serialize(_: XmlWriter, _: obj) =
+        ()
---
+++ tests/XRoadSerializer.Tests/Serialization.fs
@@ -0,0 +1,9 @@
+﻿module XRoadSerializer.Tests.Serialization
+
+open FsUnit
+open NUnit.Framework
+open XRoad
+
+let [<Test>] ``initializes new serializer`` () =
+    let serializer = Serializer()
+    serializer |> should not' (be null)
