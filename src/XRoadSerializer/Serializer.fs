329869c8f0a856281ba6e8684f92241da21717b5
Janno Põldma <janno.poldma@gmail.com>
2015-08-23 23:48:49 +0300

    Started with custom serialization.

--- src/XRoadSerializer/Serializer.fs
+++ src/XRoadSerializer/Serializer.fs
@@ -2,8 +2,30 @@
 
 open System.Xml
 
+[<RequireQualifiedAccessAttribute>]
+module XmlNamespace =
+    let [<Literal>] Http = "http://schemas.xmlsoap.org/soap/http"
+    let [<Literal>] Mime = "http://schemas.xmlsoap.org/wsdl/mime/"
+    let [<Literal>] Soap = "http://schemas.xmlsoap.org/wsdl/soap/"
+    let [<Literal>] SoapEnc = "http://schemas.xmlsoap.org/soap/encoding/"
+    let [<Literal>] SoapEnv = "http://schemas.xmlsoap.org/soap/envelope/"
+    let [<Literal>] Wsdl = "http://schemas.xmlsoap.org/wsdl/"
+    let [<Literal>] Xmime = "http://www.w3.org/2005/05/xmlmime"
+    let [<Literal>] Xml = "http://www.w3.org/XML/1998/namespace"
+    let [<Literal>] Xrd = "http://x-rd.net/xsd/xroad.xsd"
+    let [<Literal>] XRoad = "http://x-road.ee/xsd/x-road.xsd"
+    let [<Literal>] Xsd = "http://www.w3.org/2001/XMLSchema"
+    let [<Literal>] Xsi = "http://www.w3.org/2001/XMLSchema-instance"
+    let [<Literal>] Xtee = "http://x-tee.riik.ee/xsd/xtee.xsd"
+
 type Serializer() =
     member __.Deserialize(_: XmlReader) : 'T =
         null
-    member __.Serialize(_: XmlWriter, _: obj) =
-        ()
+    member __.Serialize(writer: XmlWriter, value: obj, rootName: XmlQualifiedName) =
+        match rootName.Namespace with
+        | null | "" -> writer.WriteStartElement(rootName.Name)
+        | _ -> writer.WriteStartElement(rootName.Name, rootName.Namespace)
+        match value with
+        | null -> writer.WriteAttributeString("nil", XmlNamespace.Xsi, "true")
+        | _ -> ()
+        writer.WriteEndElement()
--- tests/XRoadSerializer.Tests/Serialization.fs
+++ tests/XRoadSerializer.Tests/Serialization.fs
@@ -2,8 +2,29 @@
 
 open FsUnit
 open NUnit.Framework
+open System.IO
+open System.Text
+open System.Xml
 open XRoad
+
+module TestType =
+    type SimpleType() =
+        member val Value = 13 with get, set
 
 let [<Test>] ``initializes new serializer`` () =
     let serializer = Serializer()
-    serializer |> should not' (be null)
+    serializer |> should not' (equal null)
+
+let serialize value =
+    let serializer = Serializer()
+    let sb = StringBuilder()
+    use sw = new StringWriter(sb)
+    use writer = XmlWriter.Create(sw)
+    serializer.Serialize(writer, value, XmlQualifiedName("keha"))
+    writer.Flush()
+    sw.Flush()
+    sb.ToString()
+
+let [<Test>] ``can serialize simple value`` () =
+    let result = TestType.SimpleType() |> serialize
+    result |> should be (equal @"<?xml version=""1.0"" encoding=""utf-16""?><keha />")
