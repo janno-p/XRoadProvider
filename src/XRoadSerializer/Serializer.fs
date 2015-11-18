931b2fb26c3bcfa686751945e654698bf38e42bb
Janno Põldma <janno.poldma@gmail.com>
2015-11-18 13:42:58 +0200

    Hide IL behind quotations.

--- src/XRoadSerializer/Serializer.fs
+++ src/XRoadSerializer/Serializer.fs
@@ -2,6 +2,7 @@
 
 open FSharp.Core
 open FSharp.Quotations
+open FSharp.Quotations.DerivedPatterns
 open FSharp.Quotations.Patterns
 open System
 open System.Collections.Concurrent
@@ -28,26 +29,72 @@ module XmlNamespace =
     let [<Literal>] Xsi = "http://www.w3.org/2001/XMLSchema-instance"
     let [<Literal>] Xtee = "http://x-tee.riik.ee/xsd/xtee.xsd"
 
-module private IL =
-    let generate (il: ILGenerator) (f: Expr<XmlWriter> -> Expr<obj> -> Expr) =
-        let writer = Expr<XmlWriter>.GlobalVar("w")
-        let value = Expr<obj>.GlobalVar("o")
-        let expr = f writer value
-        let rec genIL expr =
-            match expr with
-            | Call(Some(instExpr), mi, argsExpr) when instExpr = (upcast writer) ->
-                il.Emit(OpCodes.Ldarg_0)
-                argsExpr |> List.iter (fun argExpr ->
-                    match argExpr with
-                    | Value(value, typ) when typ = typeof<string> -> il.Emit(OpCodes.Ldstr, unbox<string> value)
-                    | _ -> failwithf "Unimplemented expression: %A" argExpr)
-                il.Emit(OpCodes.Callvirt, mi)
-                il.Emit(OpCodes.Nop)
-            | _ -> failwithf "Unimplemented expression: %A" expr
-        genIL expr
-
 type SerializerDelegate = delegate of XmlWriter * obj -> unit
 type TypeMap = { Serializer: SerializerDelegate }
+
+module private IL =
+    let generate (il: ILGenerator) (property: PropertyInfo) (tmap: (TypeMap * DynamicMethod) option) (f: Expr<XmlWriter> -> Expr<obj> -> Expr<XmlWriter * obj -> unit> -> Expr<unit>) =
+        let writer = Expr.GlobalVar("w")
+        let value = Expr.GlobalVar("o")
+        let serializer = Expr.GlobalVar("s")
+        let expr = f writer value serializer
+        let rec genIL expr =
+            let genArg (argsExpr: Expr list) =
+                let rec genSingleArg (argExpr: Expr) =
+                    match argExpr with
+                    | e when e = (upcast value) ->
+                        il.Emit(OpCodes.Ldarg_1)
+                        il.Emit(OpCodes.Castclass, property.DeclaringType)
+                        il.Emit(OpCodes.Callvirt, property.GetGetMethod())
+                        if property.PropertyType.IsValueType then
+                            il.Emit(OpCodes.Box, property.PropertyType)
+                    | e when e = (upcast writer) -> il.Emit(OpCodes.Ldarg_0)
+                    | Value(null, _) -> il.Emit(OpCodes.Ldnull)
+                    | Value(value, typ) when typ = typeof<string> -> il.Emit(OpCodes.Ldstr, unbox<string> value)
+                    | Coerce(e, _) -> genSingleArg e
+                    | Call(_) -> genIL argExpr
+                    | _ -> failwithf "Unimplemented expression: %A (%A)" argExpr expr
+                argsExpr |> List.iter (genSingleArg)
+            match expr with
+            | Application(targetExpr, NewTuple(argsExpr)) when targetExpr = (serializer :> Expr) ->
+                genArg argsExpr
+                il.Emit(OpCodes.Call, tmap |> Option.get |> snd)
+                il.Emit(OpCodes.Nop)
+            | Call(Some(instExpr), mi, argsExpr) ->
+                genArg (instExpr :: argsExpr)
+                il.Emit(OpCodes.Callvirt, mi)
+                il.Emit(OpCodes.Nop)
+            | SpecificCall <@@ raise @@> (_, _, [argsExpr]) ->
+                genIL argsExpr
+                il.Emit(OpCodes.Throw)
+            | SpecificCall <@@ (=) @@> (_, _, argsExpr) ->
+                genArg argsExpr
+                il.Emit(OpCodes.Ceq)
+                il.Emit(OpCodes.Ldc_I4_0)
+                il.Emit(OpCodes.Ceq)
+            | Call(None, mi, argsExpr) ->
+                genArg argsExpr
+                il.Emit(OpCodes.Call, mi)
+            | IfThenElse(condExpr, trueExpr, falseExpr) ->
+                let lbl1 = il.DefineLabel()
+                let lbl2 = il.DefineLabel()
+                genIL condExpr
+                il.Emit(OpCodes.Brtrue_S, lbl1)
+                genIL trueExpr
+                il.Emit(OpCodes.Br_S, lbl2)
+                il.MarkLabel(lbl1)
+                il.Emit(OpCodes.Nop)
+                genIL falseExpr
+                il.MarkLabel(lbl2)
+                il.Emit(OpCodes.Nop)
+            | NewObject(ci, [argsExpr]) ->
+                genIL argsExpr
+                il.Emit(OpCodes.Newobj, ci)
+            | Sequential(expr1, expr2) ->
+                genIL expr1
+                genIL expr2
+            | _ -> failwithf "Unimplemented expression: %A" expr
+        genIL expr
 
 type Serializer() as this =
     static let typeMaps = ConcurrentDictionary<Type, TypeMap * DynamicMethod>()
@@ -55,8 +102,6 @@ type Serializer() as this =
     static do
         typeMaps.TryAdd(typeof<string>, ({ Serializer = SerializerDelegate(fun wr v -> wr.WriteValue(v)) }, null)) |> ignore
         typeMaps.TryAdd(typeof<int32>, ({ Serializer = SerializerDelegate(fun wr v -> wr.WriteValue(v)) }, null)) |> ignore
-
-    static let xmlWriteValue = typeof<XmlWriter>.GetMethod("WriteValue", [| typeof<obj> |])
 
     member __.Deserialize(_: XmlReader) : 'T =
         null
@@ -102,61 +147,38 @@ type Serializer() as this =
         | None -> failwithf "Type `%s` is not serializable." typ.FullName
 
     member private __.BuildPropertySerialization(il: ILGenerator, property: PropertyInfo) =
-        let generator' = IL.generate il
+        let typeMap = if property.PropertyType.GetCustomAttribute<XRoadTypeAttribute>() |> (isNull >> not)
+                      then Some(this.GetTypeMap(property.PropertyType))
+                      else None
+        let generator' = IL.generate il property typeMap
         let contentAttribute = property.GetCustomAttribute<XRoadContentAttribute>() |> Option.ofObj
         let elementAttribute = property.GetCustomAttribute<XRoadElementAttribute>() |> Option.ofObj
-        let isContent = contentAttribute |> Option.fold (fun _ _ -> true) false
-        let isNullable = elementAttribute |> Option.fold (fun _ x -> x.IsNullable) false
-        il.Emit(OpCodes.Nop)
-        if not isContent then
-            generator' (fun writer _ -> let propertyName = property.Name
-                                        <@@ (%writer).WriteStartElement(propertyName) @@>)
-        let endLabel =
-            if property.PropertyType.IsClass || Nullable.GetUnderlyingType(property.PropertyType) |> isNull |> not then
-                let lbl1 = il.DefineLabel()
-                il.Emit(OpCodes.Ldarg_1)
-                il.Emit(OpCodes.Castclass, property.DeclaringType)
-                il.Emit(OpCodes.Callvirt, property.GetGetMethod())
-                if property.PropertyType.IsValueType then
-                    il.Emit(OpCodes.Box, property.PropertyType)
-                il.Emit(OpCodes.Ldnull)
-                il.Emit(OpCodes.Ceq)
-                il.Emit(OpCodes.Ldc_I4_0)
-                il.Emit(OpCodes.Ceq)
-                il.Emit(OpCodes.Brtrue_S, lbl1)
-                if isNullable then
-                    let lbl2 = il.DefineLabel()
-                    il.Emit(OpCodes.Nop)
-                    generator' (fun writer _ -> <@@ (%writer).WriteAttributeString("nil", XmlNamespace.Xsi, "true") @@>)
-                    il.Emit(OpCodes.Nop)
-                    il.Emit(OpCodes.Br_S, lbl2)
-                    il.MarkLabel(lbl1)
-                    il.Emit(OpCodes.Nop)
-                    Some(lbl2)
-                else
-                    il.Emit(OpCodes.Ldstr, "Not nullable property `{0}` of type `{1}` has null value.")
-                    il.Emit(OpCodes.Ldstr, property.Name)
-                    il.Emit(OpCodes.Ldstr, property.DeclaringType.FullName)
-                    il.Emit(OpCodes.Call, typeof<String>.GetMethod("Format", [| typeof<string>; typeof<obj>; typeof<obj> |]))
-                    il.Emit(OpCodes.Newobj, typeof<Exception>.GetConstructor([| typeof<string> |]))
-                    il.Emit(OpCodes.Throw)
-                    il.MarkLabel(lbl1)
-                    None
-            else None
-        il.Emit(OpCodes.Ldarg_0)
-        il.Emit(OpCodes.Ldarg_1)
-        il.Emit(OpCodes.Castclass, property.DeclaringType)
-        il.Emit(OpCodes.Callvirt, property.GetGetMethod())
-        if property.PropertyType.IsValueType then
-            il.Emit(OpCodes.Box, property.PropertyType)
-        if property.PropertyType.GetCustomAttribute<XRoadTypeAttribute>() |> isNull then
-            if property.PropertyType = typeof<BigInteger> then
-                il.Emit(OpCodes.Callvirt, typeof<obj>.GetMethod("ToString", [| |]))
-            il.Emit(OpCodes.Callvirt, xmlWriteValue)
-        else
-            let tmap = this.GetTypeMap(property.PropertyType)
-            il.Emit(OpCodes.Call, tmap |> snd)
-        il.Emit(OpCodes.Nop)
-        match endLabel with | Some(lbl) -> il.Emit(OpCodes.Nop); il.MarkLabel(lbl) | None -> ()
-        if not isContent then
-            generator' (fun writer _ -> <@@ (%writer).WriteEndElement() @@>)
+        generator'
+            (fun writer propValue serializer ->
+                let propertyName = property.Name
+                let typeName = property.DeclaringType.FullName
+                let nullExpr =
+                    if elementAttribute |> Option.fold (fun _ x -> x.IsNullable) false
+                    then <@ (%writer).WriteAttributeString("nil", XmlNamespace.Xsi, "true") @>
+                    else <@ raise (Exception(String.Format("Not nullable property `{0}` of type `{1}` has null value.", propertyName, typeName))) @>
+                let serializeExpr =
+                    if property.PropertyType.GetCustomAttribute<XRoadTypeAttribute>() |> isNull then
+                        if property.PropertyType = typeof<BigInteger> then
+                            <@ (%writer).WriteValue((%propValue).ToString()) @>
+                        else <@ (%writer).WriteValue((%propValue)) @>
+                    else <@ (%serializer)((%writer), (%propValue)) @>
+                let contentExpr =
+                    if property.PropertyType.IsClass || Nullable.GetUnderlyingType(property.PropertyType) |> (isNull >> not) then
+                        <@
+                            if (%propValue) = null then (%nullExpr)
+                            else (%serializeExpr)
+                        @>
+                    else serializeExpr
+                if contentAttribute |> Option.fold (fun _ _ -> true) false |> not then
+                    <@
+                        (%writer).WriteStartElement(propertyName)
+                        (%contentExpr)
+                        (%writer).WriteEndElement()
+                     @>
+                else contentExpr
+                )
