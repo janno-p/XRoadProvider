373d3a3409b62ae65260822be13e5645d1bff87f
Janno Põldma <janno.poldma@gmail.com>
2016-01-16 12:30:46 +0200

    Fix build script.

--- build.fsx
+++ build.fsx
@@ -132,7 +132,7 @@ Target "CleanDocs" (fun _ ->
 Target "BuildDebug" (fun _ ->
     !! solutionFile
 #if MONO
-    |> MSBuild "" "Rebuild" [ ("Configuration", Debug"); ("DefineConstants", "MONO") ]
+    |> MSBuild "" "Rebuild" [ ("Configuration", "Debug"); ("DefineConstants", "MONO") ]
 #else
     |> MSBuildDebug "" "Rebuild"
 #endif
