ef668e8890d168d44219b99741362fd07aaa8cbd
Janno Põldma <janno.poldma@gmail.com>
2017-10-28 18:12:29 +0300

    No need for server.

--- tests/XRoadProvider.Tests/Program.fs
+++ tests/XRoadProvider.Tests/Program.fs
@@ -1,25 +1,8 @@
 module XRoadProvider.Tests.Program
 
 open Expecto
-open Suave
-open Suave.RequestErrors
 open System.Threading
 
 [<EntryPoint>]
 let main args =
-    let app =
-        choose
-            [ XRoadProvider.Tests.SerializationTest.routes
-              NOT_FOUND "Found no handlers" ]
-
-    let cts = new CancellationTokenSource()
-    let config = { defaultConfig with cancellationToken = cts.Token }
-    let listening, server = startWebServerAsync defaultConfig app
-    
-    Async.Start(server, cts.Token)
-
-    let result = runTestsInAssembly Expecto.Tests.defaultConfig args
-    
-    cts.Cancel()
-    
-    result
+    runTestsInAssembly Expecto.Tests.defaultConfig args
--- tests/XRoadProvider.Tests/SerializationTest.fs
+++ tests/XRoadProvider.Tests/SerializationTest.fs
@@ -2,26 +2,15 @@
 module XRoadProvider.Tests.SerializationTest
 
 open Expecto
-open Suave
-open Suave.Filters
-open Suave.Operators
-open Suave.Successful
 open System.Threading
+open System.Xml.Linq
 open XRoad
+open XRoad.DynamicMethods
 open XRoad.Serialization.Attributes
 open XRoadProvider.Tests.SoapUtil
 
 let [<Literal>] producerName = "producer"
 let [<Literal>] producerNamespace = "http://producer.x-road.eu/"
-
-let producerUri resourceName = sprintf "http://127.0.0.1:8080/%s" resourceName
-
-let [<Literal>] service2Response = @"<response><item /><item /></response>"
-
-let routes =
-    POST >=> choose
-        [ path "/service1" >=> OK "Hello, World!"
-          path "/service2" >=> OK (makeSoapResponse service2Response) ]
 
 module Types =
     type Type1 =
@@ -36,19 +25,23 @@ type Services =
     [<XRoadResponse("Service2Response", producerNamespace)>]
     abstract Service2: int64 -> Types.Type1[]
 
+let deserialize<'T> (nm: string) (xml: string) : 'T =
+    let map = typeof<Services>.GetMethod(nm) |> getMethodMap
+    use reader = XDocument.Parse(xml).CreateReader()
+    map.Deserialize(reader, SerializerContext()) |> unbox
+
 let [<Tests>] tests =
     testList "serialization tests" [
         test "can handle actions" {
             Expect.throwsT<exn>
-                (fun _ -> XRoadUtil.MakeServiceCall(typeof<Services>, "Service1", (producerUri "service1"), producerName, null, [||]) |> ignore)
+                (fun _ -> @"" |> deserialize<unit> "Service1")
                 "it is not implemented yet!"
         }
         
         test "can handle array type response" {
-            let response = XRoadUtil.MakeServiceCall(typeof<Services>, "Service2", (producerUri "service2"), producerName, XRoadHeader(), [||])
+            let xml = @"<response><item /><item /></response>"
+            let response = xml |> deserialize<Types.Type1[]> "Service2"
             Expect.isNotNull response "response should have value"
-            Expect.isTrue (response :? Types.Type1[]) "response should be array of Type1"
-            let arr: Types.Type1[] = unbox response
-            Expect.equal arr.Length 2 "response should have exactly 2 items"
+            Expect.equal response.Length 2 "response should have exactly 2 items"
         }
     ]
