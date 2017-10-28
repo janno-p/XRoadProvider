7aac7b5f41cfc3d88f88466a6f3aa1edf2de40fc
Janno Põldma <janno.poldma@gmail.com>
2017-10-28 14:37:37 +0300

    Suave ...

--- tests/XRoadProvider.Tests/Program.fs
+++ tests/XRoadProvider.Tests/Program.fs
@@ -1,7 +1,19 @@
 module XRoadProvider.Tests.Program
 
 open Expecto
+open Suave
+open System.Threading
 
 [<EntryPoint>]
 let main args =
-    runTestsInAssembly defaultConfig args
+    let cts = new CancellationTokenSource()
+    let config = { defaultConfig with cancellationToken = cts.Token }
+    let listening, server = startWebServerAsync defaultConfig (Successful.OK "Hello, World!")
+    
+    Async.Start(server, cts.Token)
+
+    let result = runTestsInAssembly Expecto.Tests.defaultConfig args
+    
+    cts.Cancel()
+    
+    result
--- tests/XRoadProvider.Tests/SerializationTest.fs
+++ tests/XRoadProvider.Tests/SerializationTest.fs
@@ -1,19 +1,35 @@
 module XRoadProvider.Tests.SerializationTest
 
 open Expecto
+open Suave
+open System.Threading
 open XRoad
 
 let [<Literal>] producerName = "producer"
 let [<Literal>] producerUri = "http://producer.x-road.eu/"
 
+module Types =
+    type Type1 =
+        class
+        end
+
 type Services =
     abstract Service1: unit -> unit
+    abstract Service2: int64 -> Types.Type1[]
 
 let [<Tests>] tests =
     testList "serialization tests" [
-        test "" {
+        test "can handle actions" {
             Expect.throwsT<exn>
                 (fun _ -> XRoadUtil.MakeServiceCall(typeof<Services>, "Service1", producerUri, producerName, null, [||]) |> ignore)
                 "it is not implemented yet!"
+        }
+        
+        test "can handle array type response" {
+            let response = XRoadUtil.MakeServiceCall(typeof<Services>, "Service2", producerUri, producerName, null, [||])
+            Expect.isNotNull response "response should have value"
+            Expect.isTrue (response :? Types.Type1[]) "response should be array of Type1"
+            let arr: Types.Type1[] = unbox response
+            Expect.equal arr.Length 2 "response should have exactly 2 items"
         }
     ]
