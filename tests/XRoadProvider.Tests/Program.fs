7a9f79c364f1d3e6d563389dc33ba7236315b6eb
Janno Põldma <janno.poldma@gmail.com>
2017-11-28 01:07:15 +0200

    success

--- tests/XRoadProvider.Tests/Program.fs
+++ tests/XRoadProvider.Tests/Program.fs
@@ -4,7 +4,4 @@ open Expecto
 
 [<EntryPoint>]
 let main args =
-    //SerializationTest.tests
-    //|> Test.filter (fun n -> n.EndsWith("can deserialize simple value"))
-    //|> runTests defaultConfig
     runTestsInAssembly Expecto.Tests.defaultConfig args
