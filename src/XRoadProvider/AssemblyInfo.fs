e47f9fb992360f6f041e9cead13c20745f17a8a5
Janno Põldma <janno.poldma@gmail.com>
2018-04-10 15:46:26 +0300

    Bump version to 1.0.4

--- src/XRoadProvider/AssemblyInfo.fs
+++ src/XRoadProvider/AssemblyInfo.fs
@@ -5,15 +5,15 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("XRoadProvider")>]
 [<assembly: AssemblyProductAttribute("XRoadProvider")>]
 [<assembly: AssemblyDescriptionAttribute("Type providers for generating types and service interfaces for XRoad producers.")>]
-[<assembly: AssemblyVersionAttribute("1.0.3")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.3")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.3")>]
+[<assembly: AssemblyVersionAttribute("1.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.4")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "XRoadProvider"
     let [<Literal>] AssemblyProduct = "XRoadProvider"
     let [<Literal>] AssemblyDescription = "Type providers for generating types and service interfaces for XRoad producers."
-    let [<Literal>] AssemblyVersion = "1.0.3"
-    let [<Literal>] AssemblyFileVersion = "1.0.3"
-    let [<Literal>] AssemblyInformationalVersion = "1.0.3"
+    let [<Literal>] AssemblyVersion = "1.0.4"
+    let [<Literal>] AssemblyFileVersion = "1.0.4"
+    let [<Literal>] AssemblyInformationalVersion = "1.0.4"
