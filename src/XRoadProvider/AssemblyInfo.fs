3fe10c971ed031f0f9788be4c9db533aaa55dd96
Janno Põldma <janno.poldma@gmail.com>
2019-09-03 16:52:23 +0300

    Update assembly info.

--- src/XRoadProvider/AssemblyInfo.fs
+++ src/XRoadProvider/AssemblyInfo.fs
@@ -5,15 +5,15 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("XRoadProvider")>]
 [<assembly: AssemblyProductAttribute("XRoadProvider")>]
 [<assembly: AssemblyDescriptionAttribute("Type providers for generating types and service interfaces for XRoad producers.")>]
-[<assembly: AssemblyVersionAttribute("1.0.10")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.10")>]
-[<assembly: AssemblyInformationalVersionAttribute("1.0.10")>]
+[<assembly: AssemblyVersionAttribute("1.0.11")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.11")>]
+[<assembly: AssemblyInformationalVersionAttribute("1.0.11")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "XRoadProvider"
     let [<Literal>] AssemblyProduct = "XRoadProvider"
     let [<Literal>] AssemblyDescription = "Type providers for generating types and service interfaces for XRoad producers."
-    let [<Literal>] AssemblyVersion = "1.0.10"
-    let [<Literal>] AssemblyFileVersion = "1.0.10"
-    let [<Literal>] AssemblyInformationalVersion = "1.0.10"
+    let [<Literal>] AssemblyVersion = "1.0.11"
+    let [<Literal>] AssemblyFileVersion = "1.0.11"
+    let [<Literal>] AssemblyInformationalVersion = "1.0.11"
